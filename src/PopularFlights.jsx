049124deb3147a2c2891cad2be38ffd1be55d500
Melikset Zanikov <melikset.zanikov@Meliksets-MacBook-Air.local>
2022-07-01 15:43:48 +0200

    Styling

--- src/PopularFlights.jsx
+++ src/PopularFlights.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from 'react';
 import { DateTime } from 'luxon';
+import departures from "./departures.png"
+import landing from "./landing.png"
+import calendar from "./calendar.png"
+import directFlight from "./direct-flight.png"
 
 const PopularFlights = () => {
   const [flights, setFlights] = useState([]);
@@ -21,6 +25,8 @@ const PopularFlights = () => {
 
   return (
     <div className='flights__main'>
+      <h1 className='flights__headline'>The most popular flights.</h1>
+      <h2 className='flights__subheadline'>Book now!</h2>
       <div className='flights__list'>
         {flights.map((flight) => {
           return (
@@ -35,10 +41,10 @@ const PopularFlights = () => {
               {flight.routes.length > 1 ? (
                   <p>Number of Transfers: {flight.routes.length}</p>
                 ) : (
-                  <p>Direct flight</p>
+                  <p><img className='flight__logo' src={directFlight} alt="" />Direct flight</p>
                 )}
               <p className='flight__date flight__text'>
-                {' '}
+              <img className='flight__logo' src={calendar} alt="" />
                 {DateTime.fromMillis(flight.aTime * 1000).toFormat(
                   'dd LLL yyyy'
                 )}
@@ -47,12 +53,13 @@ const PopularFlights = () => {
               </div>
 
               <div className='flight__times'>
-                <p>
-                  Departure:
+                
+                <p><img className='flight__logo' src={departures} alt="" />
                   {DateTime.fromMillis(flight.dTime * 1000).toFormat('hh:mm')}
                 </p>
-                <p>
-                  Arrival:{' '}
+
+                <p><img className='flight__logo' src={landing} alt="" />
+                  {/* Arrival:{' '} */}
                   {DateTime.fromMillis(flight.aTime * 1000).toFormat('hh:mm')}
                 </p>
               </div>
